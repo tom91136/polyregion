a71158fd0f005e596d63640ec711a1dad6be4563
Tom Lin <tom91136@gmail.com>
2022-05-22 17:24:29 +0100

    Scala public API design 3

--- frontend/examples-scala/src/main/scala/polyregion/examples/Simple.scala
+++ frontend/examples-scala/src/main/scala/polyregion/examples/Simple.scala
@@ -1,7 +1,9 @@
 package polyregion.examples
 
-import java.util.concurrent.Future
 import scala.collection.IterableOnceOps
+import scala.concurrent.Future
+import scala.concurrent.Promise
+import scala.util.Try
 
 object Foo {
   var x                         = 10
@@ -41,19 +43,38 @@ object Simple {
     enum Optimisation {
       case O0, O1, O2, O3, Ofast
     }
-    case class RuntimeOptions(arch: String, opt: Optimisation)
 
+    sealed trait Arch
+    enum CUDAArch extends Arch {
+      case SM51, SM80
+    }
 
-    trait Runtime {
+    // unknown => Option[A]
+    // known, AOT => A
+
+    sealed trait CompileOptions
+    object CompileOptions {
+      case class JIT(opt: Optimisation, archHint: Option[Arch] = None) extends CompileOptions
+      case class AOT[A <: Arch](arch: A, opt: Optimisation)            extends CompileOptions
+      case class EC()(using val ec: scala.concurrent.ExecutionContext)                                                   extends CompileOptions
+
+    }
+
+    trait Runtime[O <: CompileOptions] {
       def name: String
-      def properties(): Map[String, String]
-      def devices(): Vector[Device[RuntimeOptions]]
+      def properties: Map[String, String]
+      def devices: Vector[Device[O]]
+
+      def jit: Runtime[CompileOptions.JIT]
     }
 
     trait Device[O] {
+
+      def jit: Device[CompileOptions.JIT] = ???
+
       def name: String
       def properties(): Map[String, String]
-      def task[A](f: => A): A
+      def task[A](using o: O)(f: => A): A
 
       def foreach(x: Range)(using o: O)(f: Int => Unit): Unit
       def foreach(x: Range, y: Range)(using o: O)(f: (Int, Int) => Unit): Unit
@@ -79,32 +100,42 @@ object Simple {
 
     }
 
-    trait CompileTimeOption
-    given CompileTimeOption = new CompileTimeOption
+    trait CUDADevice extends Device[CompileOptions.AOT[CUDAArch]]
 
-    val HostRuntime: Device[RuntimeOptions]   = ???
+    class CUDARuntime extends Runtime[CompileOptions.AOT[CUDAArch]] {
+      override def name: String                    = "CUDA"
+      override def properties: Map[String, String] = Map.empty
+      override def devices: Vector[CUDADevice]     = Vector.empty
+      def jit: Runtime[CompileOptions.JIT]         = ???
 
-    val Native: Device[RuntimeOptions] = ???
+    }
 
+    val HostRuntime: Device[scala.concurrent.ExecutionContext] = ???
+    val Native: Device[CompileOptions]                         = ???
 
+    // val HIP: Runtime    = ???
+    val CUDA: CUDARuntime = ???
+    // val OpenCL: Runtime = ???
 
-    val opt       = RuntimeOptions("", Optimisation.Ofast)
-    given RuntimeOptions = opt
+    CUDA.devices(0).task(using CompileOptions.AOT(CUDAArch.SM80, Optimisation.Ofast))(1 + 1)
+    CUDA.jit.devices(0).task(using CompileOptions.JIT(Optimisation.Ofast))(1 + 1)
+
+    val p = Promise[Int]
+    p.complete(Try(1))
+    p.future
+
+    // Native.task(1+1)
+
+    implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+    HostRuntime.task(1 + 1)
+
+    given CompileOptions.JIT = CompileOptions.JIT(Optimisation.O3)
 
     Native.foreach(1 to 3)(x => ())
-    Native.foreach(1 to 3)(using opt)(x => ())
-
-
-
-    def aotDevice(s : String*) : Device[Unit]  = ???
-
-
-    aotDevice("").foreach(1 to 2){1+1}
-
-
+    Native.foreach(1 to 3)(using CompileOptions.JIT(Optimisation.O3))(x => ())
 
     Native.foreach(1 to 3, 10 to 20, 3 to 3)((x, y, z) => ())
-    Native.foreach(1 to 3, 10 to 20, 3 to 3)(using opt)((x, y, z) => ())
+    Native.foreach(1 to 3, 10 to 20, 3 to 3)(using CompileOptions.JIT(Optimisation.O3))((x, y, z) => ())
 
     trait Offload[A] extends scala.collection.Seq[A] {}
 
@@ -138,51 +169,51 @@ object Simple {
     //  singular :
 
     val l = Local
-    val result = polyregion.scala.compiletime.offload {
-//      val objRef = Foo
-//      val c      = Local.LevelA.LevelB
-//      val c2     = Foo.Bar.Baz
-//      val m      = c.a2
-//      val n      = l
-//      val a40    = l.LevelA
-//      val a41    = l.LevelA.a1
-//      val m1     = l.levelB.a2
-//
-//      val i       = n.g
-//      val objRef2 = objRef
-//      val a       = Local
-//      val a1      = a.LevelA
-//      val a2      = a.LevelA.a1
-//      val m2      = Foo.bar + objRef.nBar(2) + objRef2.nBarInline(42)
-//      val a3      = Local.LevelA.a1
-//
-//      // aliases
-//      val alias1      = Simple
-//      val alias2      = alias1
-//      val alias3      = alias2
-//      val aliasX      = alias3
-//      val aliasY      = alias3
-//      val aliasResult = aliasX.a + aliasX.b + aliasY.a + aliasY.b
-//
-//      val y =
-//        objRef.bar + Foo.baz + x + b + m + Local.g + x + Local.LevelA.a1 + Local.LevelA.LevelB.a2 + aliasResult + m1
-//      val z = 42 + x + y + Foo.nBar(y) + Foo.nBarInline(y) + Bar.y + Simple.a + Simple.b + i + objRef2.x + a1.LevelB.a2
-//      val out = a3 + m2 + a2 + m + a41 + z
-//      Foo.bar + out + Foo.nBarInline(m)
+//     val result = polyregion.scala.compiletime.offload {
+// //      val objRef = Foo
+// //      val c      = Local.LevelA.LevelB
+// //      val c2     = Foo.Bar.Baz
+// //      val m      = c.a2
+// //      val n      = l
+// //      val a40    = l.LevelA
+// //      val a41    = l.LevelA.a1
+// //      val m1     = l.levelB.a2
+// //
+// //      val i       = n.g
+// //      val objRef2 = objRef
+// //      val a       = Local
+// //      val a1      = a.LevelA
+// //      val a2      = a.LevelA.a1
+// //      val m2      = Foo.bar + objRef.nBar(2) + objRef2.nBarInline(42)
+// //      val a3      = Local.LevelA.a1
+// //
+// //      // aliases
+// //      val alias1      = Simple
+// //      val alias2      = alias1
+// //      val alias3      = alias2
+// //      val aliasX      = alias3
+// //      val aliasY      = alias3
+// //      val aliasResult = aliasX.a + aliasX.b + aliasY.a + aliasY.b
+// //
+// //      val y =
+// //        objRef.bar + Foo.baz + x + b + m + Local.g + x + Local.LevelA.a1 + Local.LevelA.LevelB.a2 + aliasResult + m1
+// //      val z = 42 + x + y + Foo.nBar(y) + Foo.nBarInline(y) + Bar.y + Simple.a + Simple.b + i + objRef2.x + a1.LevelB.a2
+// //      val out = a3 + m2 + a2 + m + a41 + z
+// //      Foo.bar + out + Foo.nBarInline(m)
 
-      var max = 0
-      var i   = 0
-      while (i < n) {
-        max = math.max(max, xs(i))
-//        max = if(i < y) max*2 else max
-//        xs(i) = i // i-1
-        i += 1
-      }
-      max
-    }
+//       var max = 0
+//       var i   = 0
+//       while (i < n) {
+//         max = math.max(max, xs(i))
+// //        max = if(i < y) max*2 else max
+// //        xs(i) = i // i-1
+//         i += 1
+//       }
+//       max
+//     }
     println(xs)
 
-    println(s"result = $result")
+    // println(s"result = $result")
     println("Done!")
   }
 
