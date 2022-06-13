4e8c425a8f37da19e984f2c678e8942b10cb7c7c
Tom Lin <tom91136@gmail.com>
2022-06-13 15:50:51 +0100

    Fix env 4

---
+++ native/setup_msvc_env.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%GITHUB_ENV%"=="" (
+  set "VC=C:\Program Files\Microsoft Visual Studio\2022\Community"
+) else (
+  set "VC=C:\Program Files\Microsoft Visual Studio\2022\Enterprise"
+)
+
+echo Using VC=%VC%
+
+set "PATH=%VC%\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja\ninja.exe;%PATH%"
+:: See https://docs.microsoft.com/en-us/cpp/build/building-on-the-command-line?view=msvc-170
+call "%VC%\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+echo Done
