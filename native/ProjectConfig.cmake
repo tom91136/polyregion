7597eb15a3caba20f24eefa5b3f882036e060471
Tom Lin <tom91136@gmail.com>
2022-12-21 19:50:57 +0000

    Bump native dependencies Fixup actions content hashing

--- native/ProjectConfig.cmake
+++ native/ProjectConfig.cmake
@@ -1,7 +1,7 @@
 
-set(LLVM_SRC_VERSION 15.0.3)
+set(LLVM_SRC_VERSION 15.0.6)
 
 set(LLVM_SOURCE_URL https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_SRC_VERSION}/llvm-project-${LLVM_SRC_VERSION}.src.tar.xz)
-set(LLVM_SOURCE_SHA256 dd07bdab557866344d85ae21bbeca5259d37b4b0e2ebf6e0481f42d1ba0fee88)
+set(LLVM_SOURCE_SHA256 29fd83b61a1171c6bd51963ca54d0d7b577255e77fcd5ff03c0548fcc5b27b4f)
 
 
