b7073d88d97318d67599bd3e6bd3765bec1d4ea1
Tom Lin <tom91136@gmail.com>
2023-03-28 05:20:37 +0100

    Add file-based memoryfs for Win32

--- native/compiler/backend/memoryfs.cpp
+++ native/compiler/backend/memoryfs.cpp
@@ -1,13 +1,27 @@
 #include "memoryfs.h"
 
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#ifdef _WIN32
+  #define WIN32_LEAN_AND_MEAN
+  #define VC_EXTRALEAN
+  #include <windows.h>
+#else
+  #include <fcntl.h>
+  #include <sys/mman.h>
+  #include <sys/stat.h>
+  #include <unistd.h>
+#endif
 
 namespace polyregion::memoryfs {
 
 std::optional<std::string> open(const std::string &name) {
+#ifdef _WIN32
+  std::wstring wname(name.begin(), name.end());
+  HANDLE h =
+      CreateFileW(wname.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+  if (h == INVALID_HANDLE_VALUE) return {};
+  CloseHandle(h);
+  return name;
+#else
   auto ns = "/" + name;
   int shmFd = shm_open(ns.data(), O_RDWR | O_CREAT, S_IRWXU);
   if (shmFd < 0) {
@@ -19,14 +33,20 @@ std::optional<std::string> open(const std::string &name) {
   }
   ::close(shmFd);
   return "/dev/shm" + ns;
+#endif
 }
 
 bool close(const std::string &name) {
+#ifdef _WIN32
+  std::wstring wname(name.begin(), name.end());
+  return DeleteFileW(wname.c_str());
+#else
   auto ns = "/" + name;
   if (shm_unlink(ns.data()) != 0) {
     return unlink(ns.data()) == 0;
   }
   return true;
+#endif
 }
 
 } // namespace polyregion::memoryfs
