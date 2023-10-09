4dfab14de0928dc21110fa038452576d7d8e358e
Tom Lin <tom91136@gmail.com>
2023-10-09 19:56:18 +0100

    Fix CI 1

--- native/setup_msvc_env.bat
+++ native/setup_msvc_env.bat
@@ -9,9 +9,17 @@ if "%GITHUB_ENV%"=="" (
 
 echo Using VC=%VC%
 
+if not "%VCPKG_ROOT%"=="" (
+  set "VCPKG_ROOT_=%VCPKG_ROOT%"
+)
+
 set "PATH=%VC%\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja\ninja.exe;%PATH%"
 :: See https://docs.microsoft.com/en-us/cpp/build/building-on-the-command-line?view=msvc-170
 call "%VC%\VC\Auxiliary\Build\vcvarsall.bat" %1
 :: X86 | AMD64
+
+if not "%VCPKG_ROOT_%"=="" (
+  set "VCPKG_ROOT=%VCPKG_ROOT_%"
+)
 
 echo Done
