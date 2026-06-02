3890b2ba40df0fc1c811063b7c64b7cce1aa361d
Tom Lin <tom91136@gmail.com>
2026-06-02 16:36:04 +0100

    ci: bump GHA deps, swap just to install-action, fix junit/diag warnings

--- native/polyfront/polyfront/diag.hpp
+++ native/polyfront/polyfront/diag.hpp
@@ -13,13 +13,13 @@ namespace polyregion::polyfront {
 template <typename Diag, std::size_t N, typename... Args>
 void emit(Diag &diag, typename Diag::Level level, const char (&fmt)[N], Args &&...args) {
   auto b = diag.Report(diag.getCustomDiagID(level, fmt));
-  (b << ... << std::forward<Args>(args));
+  (void)(b << ... << std::forward<Args>(args));
 }
 
 template <typename Diag, typename Loc, std::size_t N, typename... Args>
 void emit(Diag &diag, Loc loc, typename Diag::Level level, const char (&fmt)[N], Args &&...args) {
   auto b = diag.Report(loc, diag.getCustomDiagID(level, fmt));
-  (b << ... << std::forward<Args>(args));
+  (void)(b << ... << std::forward<Args>(args));
 }
 
 } // namespace polyregion::polyfront
