06c4deb6a22a522166f1dbc772b6de02cfe09df3
Tom Lin <tom91136@gmail.com>
2026-06-30 10:13:45 +0100

    llvm: bump to 22.1.8, rebuild caches on patch change

--- native/ProjectConfig.cmake
+++ native/ProjectConfig.cmake
@@ -1,8 +1,8 @@
 
-set(LLVM_SRC_VERSION 22.1.5)
+set(LLVM_SRC_VERSION 22.1.8)
 
 set(LLVM_SOURCE_URL https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_SRC_VERSION}/llvm-project-${LLVM_SRC_VERSION}.src.tar.xz)
-set(LLVM_SOURCE_SHA256 7972b87b705a003ce70ab55f9f0fb495d156887cba0eb296d284731139118e2c)
+set(LLVM_SOURCE_SHA256 922f1817a0df7b1489272d18134ee0087a8b068828f87ac63b9861b1a9965888)
 
 set(LLVM_PATCH_DIRS
         ${CMAKE_CURRENT_LIST_DIR}/llvm-patches
